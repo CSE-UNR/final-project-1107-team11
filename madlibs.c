532eff34295e8d1ba93e837168eacaccbb657578
1107-Welch-Donald <welchdon94@gmail.com>
2024-12-09 13:02:11 -0800

    Add files via upload
    Dons part

--- madlibs.c
+++ madlibs.c
@@ -10,14 +10,14 @@
 
 //Function declarations
 void loadGame(char game[][MAX_LENGTH], int *lineCount); // KACE
-void userInput(char userInput[MAX_WORDS][MAX_LENGTH], int lineCount); // DON
-void replaceIndex(char game[][MAX_LENGTH], char userInput[MAX_WORDS][MAX_LENGTH], int lineCount); // TAYLOR
+void userInput(char user_inputs[MAX_WORDS][MAX_LENGTH], int lineCount, char game[][MAX_LENGTH]); // DON
+void replaceIndex(char game[][MAX_LENGTH], char user_inputs[MAX_WORDS][MAX_LENGTH], int lineCount); // TAYLOR
 void printGame(char game [][MAX_LENGTH], int lineCount); // KACE
 void initialize_arr(char arr[][MAX_LENGTH], int size); // DON
-void initialize_userInput(char userInput [MAX_WORDS][MAX_LENGTH], int size); // TAYLOR
+void initialize_user_inputs(char user_inputs [MAX_WORDS][MAX_LENGTH], int size); // TAYLOR
 void playGame(char game[][MAX_LENGTH], char userInput[MAX_WORDS][MAX_LENGTH]); // TAYLOR
 
-int main() { // KACE
+int main(){ //KACE
 	char game[MAX_WORDS][MAX_LENGTH];
 	int lineCount = 0;
 
@@ -58,8 +58,7 @@ FILE *file = fopen("madlib2.txt", "w"); // Open the file for writing
 
 return 0;
 }
-
-// Function to load the Mad Libs game template into memory
+ // Function to load the Mad Libs game template into memory
 void loadGame(char game[][MAX_LENGTH], int *lineCount) { // KACE
 	FILE *file = fopen("madlib2.txt", "r"); // Open the file for reading
 		if (file == NULL) {
@@ -79,11 +78,47 @@ int count = 0;
 fclose(file);
 *lineCount = count; // Update the line count
 }
-
+//Function to get user input for the game
+void userInput(char user_inputs[MAX_WORDS][MAX_LENGTH], int lineCount, char game[][MAX_LENGTH]){
+	char placeholder;
+	for(int i = 0; i < lineCount; i++){
+	//Checks for placeholders and prompts the user
+		for(int j = 0; game[i][j] != '\0'; j++){
+			placeholder = game[i][j];
+			if(placeholder == 'A' || placeholder == 'N' || placeholder == 'V'){
+			   if(placeholder == 'A'){
+			   	printf("Enter an adjective: ");
+			   }else if(placeholder == 'N'){
+			   	printf("Enter a noun: ");
+			   }else if(placeholder == 'V'){
+			   	printf("Enter a verb: ");
+			   }
+			   fgets(user_inputs[i], MAX_LENGTH, stdin);
+			   for(int k = 0; k < MAX_LENGTH; k++){
+			   	if(user_inputs[i][k] == '\n'){
+			   	   user_inputs[i][k] = '\0';
+			   	   break;
+			   	   }
+		   	  } 			
+	   	 }
+	}
+   }
+}        
 // Function to print the Mad Libs game template
 void printGame(char game[][MAX_LENGTH], int lineCount) { // KACE
 	for (int i = 0; i < lineCount; i++) {
 		printf("%s", game[i]); // Print each line
 	}
 }
+			 	
+//Function to initialize 2d Array
+void initialize_arr(char arr[][MAX_LENGTH], int size){
+	for(int i = 0; i < size; i++){
+		arr[i][0] = '\0';// Initializes an empty string	
+               }
+}	         											
+		 
+
+
+
 
